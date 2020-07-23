e86f7cac10467b1255400a644da86d615f3365f5
xuruifeng <xuruifeng@xianmaism.com>
2020-07-23 18:22:52 +0800

    引入gatewayclient

---
+++ app/Services/WorkerMan.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Services;
+
+
+use GatewayClient\Gateway;
+
+class WorkerMan extends Base
+{
+    protected $user;
+
+    public function webSocketConnect($client_id, $data)
+    {
+    }
+
+    public function sendMessage($client_id, $message)
+    {
+    }
+
+    public function closeConnect($client_id)
+    {
+    }
+}
