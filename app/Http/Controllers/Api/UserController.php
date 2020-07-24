05d5a1e0ad79a141f64b7e32541ba98ade83d1cb
xuruifeng <xuruifeng@xianmaism.com>
2020-07-24 16:41:02 +0800

    用户注册

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,13 +3,51 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Services\UserService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+    protected $UserService;
+    protected $rets;
+
+    public function __construct()
+    {
+        $this->UserService = new UserService();
+        $this->rets = [
+            'code' => 1000,
+            'message' => '',
+            'data' => [],
+        ];
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function register(Request $request)
     {
-        
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:32'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
+        ]);
+
+        if ($validator->fails()) {
+            $this->rets['code'] = 1001;
+            $this->rets['message'] = $validator;
+            return response()->json($this->rets);
+        }
+        try {
+            $data = $this->UserService->register($request);
+            $this->rets['data'] = $data;
+        }catch (\Exception $exception){
+            $this->rets['code'] = 1001;
+            $this->rets['message'] = $exception->getMessage();
+            return response()->json($this->rets);
+        }
+        return response()->json($this->rets);
     }
 
     public function login(Request $request)
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,7 +4,37 @@
 namespace App\Services;
 
 
-class UserService
-{
+use App\Models\User;
+use Illuminate\Support\Str;
 
+class UserService extends BaseService
+{
+    /**
+     * @param $request
+     * @return array
+     * @throws \Exception
+     */
+    public function register($request)
+    {
+        if($user = User::where('name', $request->name)->first()){
+            throw new \Exception('用户名已存在');
+        }
+        if($user = User::where('email', $request->email)->first()){
+            throw new \Exception('邮箱已被注册');
+        }
+        $uuid = Str::uuid();
+        $add_data = [
+            'uuid' => $uuid,
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => encrypt($request->password)
+        ];
+
+        User::insert($add_data);
+
+        return [
+            'token' => $uuid,
+            'name' => $request->name,
+        ];
+    }
 }
