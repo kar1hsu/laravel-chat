6dc06915ad5e49758f407343cfeba08393cc5a9a
xuruifeng <xuruifeng@xianmaism.com>
2020-07-24 16:07:08 +0800

    新增用户模型

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -4,7 +4,7 @@
 namespace App\Services;
 
 
-class Base
+class BaseService
 {
 
 }
--- app/Services/WorkerManService.php
+++ app/Services/WorkerManService.php
@@ -6,7 +6,7 @@ namespace App\Services;
 
 use GatewayClient\Gateway;
 
-class WorkerMan extends Base
+class WorkerManService extends BaseService
 {
     protected $user;
 
@@ -21,28 +21,32 @@ class WorkerMan extends Base
         if (!$message_data) {
             return 0;
         }
+        // uuid加密后生成的token
         $token = $message_data['token'];
         try {
+            // 解密uuid
             $uuid = decrypt($token);
             $user = $uuid;
         } catch (\Exception $exception) {
             $return['code'] = 9999;
             $return['msg'] = 'un login';
+            // 提示用户登录并关闭当前链接
             Gateway::sendToClient($client_id, json_encode($return));
             Gateway::closeClient($client_id);
             return 0;
         }
 
         switch ($message_data['type']) {
-            case 'ping':
+            case 'ping': // 心跳
                 break;
-            case 'login':
+            case 'login': // 登录
+                // 绑定uuid并设置session
                 Gateway::bindUid($client_id, $uuid);
                 Gateway::setSession($client_id, $user);
                 break;
-            case 'send':
+            case 'send': // 发送信息
                 switch ($message_data['send_type']) {
-                    case 'friend':
+                    case 'friend': // 发送给好友
                         $new_message = array(
                             'type' => 'send',
                             'send_type' => 'friend',
@@ -52,14 +56,16 @@ class WorkerMan extends Base
                             'content' => "<b>say: </b>" . nl2br(htmlspecialchars($message_data['content'])),
                             'time' => date('Y-m-d H:i:s'),
                         );
+                        // 根据to_client_id实际是用户的uuid,根据uuid去获取绑定的client_id
                         $to_client_ids = Gateway::getClientIdByUid($message_data['to_client_id']);
                         if ($to_client_ids) {
                             foreach ($to_client_ids as $client_id) {
+                                // 发送消息
                                 Gateway::sendToClient($client_id, json_encode($new_message));
                             }
                         }
                         break;
-                    case 'room':
+                    case 'room': // 发送到群
                         $new_message = array(
                             'type' => 'send',
                             'send_type' => 'room',
@@ -69,6 +75,7 @@ class WorkerMan extends Base
                             'content' => "<b>say: </b>" . nl2br(htmlspecialchars($message_data['content'])),
                             'time' => date('Y-m-d H:i:s'),
                         );
+                        // 发送消息
                         Gateway::sendToGroup($message_data['room_id'], json_encode($new_message));
                         break;
                 }
